7967941d6d14c4d55d1b10d78e9ba7b5e0d6c100
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2021-05-15 00:09:07 -0300

    refactor: extract arg parser to config struct

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+    println!("Searching for {}", config.query);
+    println!("In File {}", config.filename);
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In File {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
     println!("With text: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
