6d91ea6b57c5e3391dcdada308647b9dd8c6e9c9
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2021-05-22 11:28:27 -0300

    refactor: replace loop by iterators on search functions

--- src/lib.rs
+++ src/lib.rs
@@ -47,27 +47,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result: Vec<&'a str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
+    return contents
+        .lines()
+        .filter(|line| line.contains(&query))
+        .collect();
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
 
-    let mut result: Vec<&'a str> = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-
-    result
+    return contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect();
 }
 
 #[cfg(test)]
